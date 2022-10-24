ab9e3d65ddc0c8c963b2fd521f372bfc8596f205
linas92 <undeadarmyleader@gmail.com>
2022-10-24 11:26:31 +0300

    sudeliojau klases.Dabar researching foreign keys

--- drugs.py
+++ drugs.py
@@ -10,7 +10,51 @@ Base = declarative_base()
 # 2 - kasininkas
 # 3 - kompanija
 # 4 - vaistas
-# 5 - isveziotojas
+
+class Customer(Base):
+    __tablename__ = "customer"
+    id = Column(Integer, primary_key = True)
+    name = Column("name", String)
+    last_name = Column("last_name", String)
+    phone_number = Column("phone_number", Integer)
+    # cashier_id = relationship("Cashier", back_populates = "cashier")
+
+    def __repr__(self):
+        return f"({self.id}, {self.name}, {self.last_name}, {self.phone_number}, )"
+    
+
+class Cashier(Base):
+    __tablename__ = "cashier"
+    id = Column(Integer, primary_key = True)
+    name = Column("name", String)
+    last_name = Column("last_name", String)
+    # drug_id = relationship("Company", back_populates = "company")
+
+    def __repr__(self):
+        return f"({self.id}, {self.name}, {self.last_name})"
+
+
+class Company(Base):
+    __tablename__ = "company"
+    id = Column(Integer, primary_key = True)
+    name = Column("name", String)
+    address = Column("address", String)
+    telephone = Column("telephone", Integer)
+    # drug_id = relationship("Vitamin", back_populates = "vitanim")
+
+    def __repr__(self):
+        return f"({self.id}, {self.name}, {self.address}, {self.telephone})"
+
+
+class Vitamin(Base):
+    __tablename__ = "vitamin"
+    id = Column(Integer, primary_key = True)
+    name = Column("name", String)
+    # drug_id = relationship("Company", back_populates = "company")
+
+    def __repr__(self):
+        return f"({self.id}, {self.name}, )"
+
 
 
 if __name__ == "__main__":
