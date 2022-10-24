8b929e054f8547797fafe1055b7a168dfc7fbce7
linas92 <undeadarmyleader@gmail.com>
2022-10-24 09:56:28 +0300

    projhect set up

---
+++ drugs.py
@@ -0,0 +1,3 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine,Table, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
