810ab91549d1e259e8d40ad7bff099177526f3ba
linas92 <undeadarmyleader@gmail.com>
2022-10-24 11:46:06 +0300

    adding text instructions for "demo" trial

--- crud.py
+++ crud.py
@@ -9,4 +9,19 @@ def create_object(object_class, **kwargs):
     session.commit()
     return object
 
-    
+if __name__ == "__main__":
+    while True:
+        print("==================================================================================")
+        print("Use your keys to insert data:")
+        print("1 - Customers name")
+        print("2 - reviews customers n stuff")
+        print("3 - ")
+        print("==================================================================================")
+        pasirinkimas = int(input())
+        if pasirinkimas == 1:
+            create_object(
+                Customer, 
+                name = input("Enter the customers name: "), 
+                last_name = input("Enter the customers last name: "), 
+                phone_number = int(input("Enter the customers phone number: ")),
+            )
