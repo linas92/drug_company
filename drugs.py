c13171b0e43970309350c7189dd29cbf9a3c2165
linas92 <undeadarmyleader@gmail.com>
2022-10-24 10:11:26 +0300

    plano susideliojimas kaip ka darysiu

--- drugs.py
+++ drugs.py
@@ -1,3 +1,18 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, create_engine,Table, Float
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine,Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+
+engine = create_engine("sqlite:///data/drugs.db")
+Base = declarative_base()
+
+# susikursiu keleta klasiu ir lenteliu:
+# 1 - vartotojas
+# 2 - kasininkas
+# 3 - kompanija
+# 4 - vaistas
+# 5 - isveziotojas
+
+
+if __name__ == "__main__":
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
